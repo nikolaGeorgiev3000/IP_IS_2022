c8d35e447382ef2f9a84c6d3eb7905058a720657
NickGeorgiev <ngxt3000@gmail.com>
2022-11-12 20:31:49 +0200

    Modified naming variables

--- AdditionalExercise/infTest_01_task02.cpp
+++ AdditionalExercise/infTest_01_task02.cpp
@@ -7,13 +7,13 @@ int main()
     long long n;
     cout << "n = ";
     cin >> n;
-    long long impDigit, currentBiggest = 0, counter = 1, currentDigit = n % 10;
+    long long impDigit, currentBiggest = 0, counter = 1, prevDigit = n % 10;
     n /= 10;
     while (n)
     {
 
-        long long helpDigit = n % 10;
-        if (currentDigit == helpDigit)
+        long long currentDigit = n % 10;
+        if (prevDigit == currentDigit)
         {
             counter++;
         }
@@ -24,10 +24,10 @@ int main()
             {
                 currentBiggest = counter;
                 counter = 0;
-                impDigit = currentDigit;
+                impDigit = prevDigit;
             }
             counter = 1;
-            currentDigit = n % 10;
+            prevDigit = n % 10;
         }
         n /= 10;
     }
@@ -35,7 +35,7 @@ int main()
     {
         currentBiggest = counter;
         counter = 0;
-        impDigit = currentDigit;
+        impDigit = prevDigit;
     }
     cout << "Length: " << currentBiggest << " Digit: (" << impDigit << ")";
 
